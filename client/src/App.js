3b1dd6b37be40b645ed38c057829a5f4e8b6f295
HOLFF <leerharter@gmail.com>
2021-02-15 11:40:59 +0100

    initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href='http://localhost:8888'>
+        <button>Log in with Spotify</button>
+      </a>
+    </div>
+  );
+}
+
+export default App;
