a2b900018421f746477fe913b060c76a04b2493f
LukHtl2 <lueg229@gmail.com>
2021-02-17 10:31:42 +0100

    funktionen added

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,15 @@ class App extends Component {
     super();
     const params = this.getHashParams();
     const token = params.access_token;
-    if (token) {
-      spotifyApi.setAccessToken(token);
+    if (true) {
+      spotifyApi.setAccessToken("BQDJ78UMr0ieSfsAFidog_D8Ek41Voco7qM--v6HAdcSKzOWLuiOXHfWKiMDK4CHtYuZo1JNlIICQABD67yHEWGGGr8U2FKTHO_e9zCYVf647b6RhNc6qaRpjVoaQivNqQ7BwvowSiHLqLg_wBEOnIOWye2doVRcmQKXiQLiBVvrVn5AWMqfCchJqPwq&refresh_token=AQA6l1WL8PXv3Xxcav9Bl7-ygFX8-EAp9Ro2nplbyCdffq5mGkyPI8D9Fh1-PX2NbwZZy_S0hcFYiUoni-B2fa7UzpKObZToEFmzXimO1_rmAmZ4KVEB0xC9-Bt9pM5nNbQ");
     }
     this.state = {
-      loggedIn: token ? true : false,
-      nowPlaying: { name: 'Not Checked', albumArt: '' }
+      loggedIn:  true,
+      nowPlaying: { name: 'Not Checked', albumArt: '' },
+      userPlayList: {name: 'Not Checked'},
+      playListTracks: {name: 'Not Checked'},
+      trackToPlayList: {name: 'Not Checked'}
     }
   }
   getHashParams() {
@@ -40,6 +43,42 @@ class App extends Component {
             }
         });
       })
+  }
+
+  getPlayLists(){
+    spotifyApi.getUserPlaylists()
+      .then((response) => {
+        this.setState({
+          userPlayList: {
+              name: response.item.name,
+              playList: response.item.name
+            }    
+        });
+      })  
+  }
+
+  getTracksOfPlayList(){ 
+    spotifyApi.getPlaylistTracks()
+      .then((response) => {
+        this.setState({
+          playListTracks:{
+            name:response.item.name,
+            tracks: respone.item.name     
+            }
+        });
+      }) 
+  }
+
+  addTrackToPlayList(){
+    spotifyApi.addTracksToPlaylist()
+      .then((response) => {
+        this.setState({
+          trackToPlayList: {   
+              name: response.item.name,
+              addedTrackToPlayList: response.item.name
+            }    
+        });
+      })  
   }
 
   skipToNext(){
