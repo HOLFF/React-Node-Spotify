d0d42fa333f584d703a5d0eb38c9ed38a8728519
HOLFF <leerharter@gmail.com>
2021-03-10 09:30:10 +0100

    rm secret and id

--- server-auth/authorization_code/app.js
+++ server-auth/authorization_code/app.js
@@ -14,6 +14,7 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
 
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
